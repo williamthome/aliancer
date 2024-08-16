4635462bb5f71c4908e04b18700126adbb736e3d
William Fank Thomé <willegp88@gmail.com>
2024-08-15 22:15:53 -0300

    Feature first dashboard layout

--- assets/js/hooks/Chart.js
+++ assets/js/hooks/Chart.js
@@ -4,14 +4,27 @@ export default Chart = {
   mounted() {
     const options = {
       chart: {
-        type: 'line'
+        type: 'line',
+        parentHeightOffset: 0,
       },
       series: [{
-        name: 'sales',
         data: [30,40,35,50,49,60,70,91,125]
       }],
       xaxis: {
-        categories: [1991,1992,1993,1994,1995,1996,1997,1998,1999]
+        categories: [1991,1992,1993,1994,1995,1996,1997,1998,1999],
+      },
+      yaxis: {
+        labels: {
+          offsetX: -16
+        }
+      },
+      grid: {
+        padding: {
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0
+        }
       }
     }
 
