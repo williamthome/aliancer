895917726de09c3be87c656addd63e1a5475757d
William Fank Thomé <willegp88@gmail.com>
2024-08-20 00:12:16 -0300

    Create admin users via seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+## Create admin users
+
+now = DateTime.utc_now() |> DateTime.truncate(:second)
+
+String.split(System.get_env("ALIANCER_ADMIN_EMAILS"), ";")
+|> Enum.each(fn email ->
+  %Aliancer.Accounts.User{
+    email: email,
+    hashed_password: Bcrypt.hash_pwd_salt(System.get_env("ALIANCER_ADMIN_PASSWORD")),
+    confirmed_at: now,
+    is_admin: true
+  }
+  |> Aliancer.Repo.insert(
+    on_conflict: [set: [is_admin: true, updated_at: DateTime.utc_now()]],
+    conflict_target: :email
+  )
+end)
